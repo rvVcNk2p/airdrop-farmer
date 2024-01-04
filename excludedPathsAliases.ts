34217ab601f84b88d332c6be5f7195427b557bfa
Kussy Leo <kussynorbertleo@gmail.com>
2024-01-05 00:44:47 +0100

    #28: Sentry added v1.3

---
+++ excludedPathsAliases.ts
@@ -0,0 +1,12 @@
+const excludedCoveragePathAliases = [
+	// Sentry
+	'src/app/sentry-example-page/page.jsx',
+	'next.config.js',
+	'sentry.client.config.ts',
+	'src/app/global-error.jsx',
+	'sentry.edge.config.ts',
+	'sentry.server.config.ts',
+	'src/app/api/sentry-example-api/route.js',
+]
+
+export default excludedCoveragePathAliases
