d333d942f79993bf5ed70d541ec21ffb68dc4f70
Kussy Leo <kussynorbertleo@gmail.com>
2024-01-05 12:38:51 +0100

    #31: Testing coverage increased v1.2.1

--- __tests__/landing/8_ChooseYour.test.jsx
+++ __tests__/landing/8_ChooseYour.test.jsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react'
 
-import ChooseYour from '../../src/modules/landing/components/8_ChooseYour/Faq'
+import ChooseYour from '../../src/modules/landing/components/8_ChooseYour/ChooseYour'
 
 describe('ChooseYour', () => {
 	test.each([['Pricing', 'Eager to boost your profits']])(
