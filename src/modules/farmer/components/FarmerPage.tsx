e7e2b0d6b9032c79c9b69ad09cc4b90c5fbb9a93
Kussy Leo <kussynorbertleo@gmail.com>
2024-02-07 10:10:01 +0100

    Husky test

--- src/modules/farmer/components/FarmerPage.tsx
+++ src/modules/farmer/components/FarmerPage.tsx
@@ -3,8 +3,8 @@ import { StrategySection } from '@modules/farmer/components/StrategySection'
 
 export const FarmerPage = () => {
 	return (
-		<div className="flex flex-col items-center gap-4">
-			<GroupSection />
+		<div className="flex-col items-center gap-4 flex">
+			{/* <GroupSection /> */}
 			<StrategySection />
 		</div>
 	)
