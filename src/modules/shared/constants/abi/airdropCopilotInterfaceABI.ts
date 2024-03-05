9bb83e2280cc40692c241963d10bfae5d37e5d69
Kussy Leo <kussynorbertleo@gmail.com>
2024-03-05 07:14:22 +0100

    feat(app): ABI contract updated

--- src/modules/shared/constants/abi/airdropCopilotInterfaceABI.ts
+++ src/modules/shared/constants/abi/airdropCopilotInterfaceABI.ts
@@ -1,79 +1,173 @@
 export const airdropCopilotInterfaceABI = [
 	{
 		inputs: [
-			{ internalType: 'address', name: '_owner', type: 'address' },
-			{ internalType: 'address', name: '_manager', type: 'address' },
+			{
+				internalType: 'address',
+				name: '_owner',
+				type: 'address',
+			},
+			{
+				internalType: 'address',
+				name: '_manager',
+				type: 'address',
+			},
 		],
 		stateMutability: 'nonpayable',
 		type: 'constructor',
 	},
 	{
 		inputs: [
-			{ internalType: 'address', name: 'userAddress', type: 'address' },
-			{ internalType: 'uint8', name: 'discountPercentage', type: 'uint8' },
+			{
+				internalType: 'address',
+				name: 'userAddress',
+				type: 'address',
+			},
+			{
+				internalType: 'uint8',
+				name: 'discountPercentage',
+				type: 'uint8',
+			},
 		],
 		name: 'addDiscount',
 		outputs: [],
-		stateMutability: 'payable',
+		stateMutability: 'nonpayable',
 		type: 'function',
 	},
 	{
 		inputs: [
-			{ internalType: 'uint64', name: 'userPrice', type: 'uint64' },
-			{ internalType: 'uint8', name: 'discount', type: 'uint8' },
+			{
+				internalType: 'uint64',
+				name: 'userPrice',
+				type: 'uint64',
+			},
+			{
+				internalType: 'uint8',
+				name: 'discount',
+				type: 'uint8',
+			},
 		],
 		name: 'calculateDiscount',
-		outputs: [{ internalType: 'uint64', name: '', type: 'uint64' }],
+		outputs: [
+			{
+				internalType: 'uint64',
+				name: '',
+				type: 'uint64',
+			},
+		],
 		stateMutability: 'pure',
 		type: 'function',
 	},
 	{
-		inputs: [{ internalType: 'address', name: 'newManager', type: 'address' }],
+		inputs: [
+			{
+				internalType: 'address',
+				name: 'newManager',
+				type: 'address',
+			},
+		],
 		name: 'changeManager',
 		outputs: [],
-		stateMutability: 'payable',
+		stateMutability: 'nonpayable',
 		type: 'function',
 	},
 	{
-		inputs: [{ internalType: 'address', name: '', type: 'address' }],
+		inputs: [
+			{
+				internalType: 'address',
+				name: '',
+				type: 'address',
+			},
+		],
 		name: 'discounts',
-		outputs: [{ internalType: 'uint8', name: '', type: 'uint8' }],
+		outputs: [
+			{
+				internalType: 'uint8',
+				name: '',
+				type: 'uint8',
+			},
+		],
 		stateMutability: 'view',
 		type: 'function',
 	},
 	{
-		inputs: [{ internalType: 'address', name: 'userAddress', type: 'address' }],
+		inputs: [
+			{
+				internalType: 'uint256',
+				name: 'amount',
+				type: 'uint256',
+			},
+		],
+		name: 'gatherDeposit',
+		outputs: [],
+		stateMutability: 'nonpayable',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'address',
+				name: 'userAddress',
+				type: 'address',
+			},
+		],
 		name: 'isSubscriptionActive',
-		outputs: [{ internalType: 'bool', name: '', type: 'bool' }],
+		outputs: [
+			{
+				internalType: 'bool',
+				name: '',
+				type: 'bool',
+			},
+		],
 		stateMutability: 'view',
 		type: 'function',
 	},
 	{
 		inputs: [],
 		name: 'manager',
-		outputs: [{ internalType: 'address', name: '', type: 'address' }],
+		outputs: [
+			{
+				internalType: 'address',
+				name: '',
+				type: 'address',
+			},
+		],
 		stateMutability: 'view',
 		type: 'function',
 	},
 	{
 		inputs: [],
 		name: 'owner',
-		outputs: [{ internalType: 'address', name: '', type: 'address' }],
+		outputs: [
+			{
+				internalType: 'address',
+				name: '',
+				type: 'address',
+			},
+		],
 		stateMutability: 'view',
 		type: 'function',
 	},
 	{
 		inputs: [
 			{
+				internalType: 'address',
+				name: '_address',
+				type: 'address',
+			},
+			{
 				internalType: 'enum Subscription.TIER_TYPE',
-				name: 'tierType',
+				name: '_tierType',
 				type: 'uint8',
 			},
-			{ internalType: 'uint32', name: 'additionalDuration', type: 'uint32' },
+			{
+				internalType: 'uint32',
+				name: '_validity',
+				type: 'uint32',
+			},
 		],
-		name: 'setTierAdditionalDuration',
+		name: 'setSubscription',
 		outputs: [],
-		stateMutability: 'payable',
+		stateMutability: 'nonpayable',
 		type: 'function',
 	},
 	{
@@ -83,11 +177,33 @@ export const airdropCopilotInterfaceABI = [
 				name: 'tierType',
 				type: 'uint8',
 			},
-			{ internalType: 'uint64', name: 'newBasePriceWei', type: 'uint64' },
+			{
+				internalType: 'uint32',
+				name: 'additionalDuration',
+				type: 'uint32',
+			},
+		],
+		name: 'setTierAdditionalDuration',
+		outputs: [],
+		stateMutability: 'nonpayable',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'enum Subscription.TIER_TYPE',
+				name: 'tierType',
+				type: 'uint8',
+			},
+			{
+				internalType: 'uint64',
+				name: 'newBasePriceWei',
+				type: 'uint64',
+			},
 		],
 		name: 'setTierPriceWei',
 		outputs: [],
-		stateMutability: 'payable',
+		stateMutability: 'nonpayable',
 		type: 'function',
 	},
 	{
@@ -104,7 +220,13 @@ export const airdropCopilotInterfaceABI = [
 		type: 'function',
 	},
 	{
-		inputs: [{ internalType: 'address', name: '', type: 'address' }],
+		inputs: [
+			{
+				internalType: 'address',
+				name: '',
+				type: 'address',
+			},
+		],
 		name: 'subscriptions',
 		outputs: [
 			{
@@ -112,7 +234,11 @@ export const airdropCopilotInterfaceABI = [
 				name: 'tierType',
 				type: 'uint8',
 			},
-			{ internalType: 'uint32', name: 'validity', type: 'uint32' },
+			{
+				internalType: 'uint32',
+				name: 'validity',
+				type: 'uint32',
+			},
 		],
 		stateMutability: 'view',
 		type: 'function',
@@ -127,8 +253,16 @@ export const airdropCopilotInterfaceABI = [
 		],
 		name: 'tiers',
 		outputs: [
-			{ internalType: 'uint64', name: 'priceWei', type: 'uint64' },
-			{ internalType: 'uint32', name: 'additionalDuration', type: 'uint32' },
+			{
+				internalType: 'uint64',
+				name: 'priceWei',
+				type: 'uint64',
+			},
+			{
+				internalType: 'uint32',
+				name: 'additionalDuration',
+				type: 'uint32',
+			},
 		],
 		stateMutability: 'view',
 		type: 'function',
