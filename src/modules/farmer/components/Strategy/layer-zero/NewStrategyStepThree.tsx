8475969396d3c894fbc49c4a974a332c17723d28
Kussy Leo <kussynorbertleo@gmail.com>
2024-03-08 10:09:38 +0100

    feat(app): Typo fixed

--- src/modules/farmer/components/Strategy/layer-zero/NewStrategyStepThree.tsx
+++ src/modules/farmer/components/Strategy/layer-zero/NewStrategyStepThree.tsx
@@ -155,7 +155,7 @@ export const NewStrategyStepThree = ({
 						<Label className="leading-6">
 							4. Your <span className="text-valid">USDT</span> ot{' '}
 							<span className="text-valid">USDC</span> balance needs to be the
-							<span className="text-valid">BIGGEST</span>
+							<span className="text-valid"> BIGGEST </span>
 							between all other currencies.
 							<div>
 								<span className="text-valid">For example</span>, if you have $23
